b9b808001e14bc304b5de50544aec43f5e9431ab
ayoub <ayoub10hamaoui@gmail.com>
2019-02-20 02:37:07 +0100

    nothing special

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,10 +12,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/search?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c',{
-      credentials: 'include',
-      mode: 'cors',
-    })
+    fetch('http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/search?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c')
       .then(res => res.json())
       .then(json => this.setState({ 
         isLoaded: true,
